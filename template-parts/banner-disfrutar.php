a8d65a728e95d1732b4bb124b03dcd519ec7252c
Luis Carlos Pando <hey@luiscarlospando.com>
2026-07-23 16:25:52 -0600

    Ajustes cliente

---
+++ template-parts/banner-disfrutar.php
@@ -0,0 +1,56 @@
+<!-- Banner: disfrutar -->
+<section id="banner-2" class="py-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 text-center">
+                <p
+                    id="text-aux-1"
+                    class="mb-0"
+                    data-aos="fade-right"
+                    data-aos-duration="1000"
+                >
+                    Tu única tarea es
+                </p>
+                <h1
+                    data-aos="fade-in"
+                    data-aos-duration="1000"
+                    data-aos-delay="400"
+                >
+                    disfrutar
+                </h1>
+                <p
+                    id="text-aux-2"
+                    data-aos="fade-left"
+                    data-aos-duration="1000"
+                    data-aos-delay="200"
+                >
+                    Aquí no te preocupes por nada.
+                </p>
+                <a
+                    class="btn btn-light rounded-pill"
+                    href="https://wa.me/526142218487?text=<?php echo rawurlencode(
+                        "¡Hola " . get_bloginfo("name") . "! Quiero cotizar un evento.",
+                    ); ?>"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="600"
+                    >Cotizar evento</a
+                >
+            </div>
+        </div>
+    </div>
+    <div
+        id="banner-pizza-1"
+        class="d-none d-md-block"
+        data-aos="fade-right"
+        data-aos-duration="1000"
+        data-aos-delay="800"
+    ></div>
+    <div
+        id="banner-pizza-2"
+        class="d-none d-md-block"
+        data-aos="fade-left"
+        data-aos-duration="1000"
+        data-aos-delay="800"
+    ></div>
+</section>
